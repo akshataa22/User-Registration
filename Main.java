e0c92daff0e3ded45a10365f35b5aee8ad77f50f
akshataa22 <dhanawadeakshata53@gmail.com>
2023-12-08 01:40:10 +0530

    Mobile Number check from user

--- Main.java
+++ Main.java
@@ -20,8 +20,15 @@ public class Main {
 
         System.out.println("Enter email id: ");
         boolean isValidEmailId = Pattern.matches("^[a-z]{0,40}[@]{1}[a-z]{0,15}[.]{1}[a-z]{5}",sc.nextLine());
-        if(isValidEmailId==false){
+        if(isValidEmailId == false){
             System.out.println("Please enter valid email id.");
         }
+
+        System.out.println("Enter mobile number: ");
+        boolean isValidMblNumber = Pattern.matches("^[0-9]{2}\s[0-9]{10}",sc.nextLine());
+        if(isValidMblNumber == false){
+            System.out.println("Please enter valid mobile number - Eg. 91 9919819801, Country code followed by space and 10 digit number");
+        }
+        
     }
 }
