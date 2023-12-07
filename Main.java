46f631d4a690546f80299b6d816df4f727cdb24e
akshataa22 <dhanawadeakshata53@gmail.com>
2023-12-08 01:31:24 +0530

    First Name Check from user

---
+++ Main.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first Name: ");
+        boolean isValidFirstName = Pattern.matches("^[A-Z]{1}[a-z]{0,20}",sc.nextLine());
+        if(isValidFirstName == false){
+            System.out.println("Please enter valid name first name (starting with Capital letter and has minimum 3 letters).");
+        }
+    }
+}
